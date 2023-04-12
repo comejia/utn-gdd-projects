9be6e9969956ca936a73caa65b89cdfa6ee41761
Cesar <cmejia@frba.utn.edu.ar>
2023-04-12 19:34:54 -0300

    Ej 2

--- Productos/productos_sql.sql
+++ Productos/productos_sql.sql
@@ -5,3 +5,11 @@ SELECT c.clie_codigo, c.clie_razon_social
 FROM Cliente c
 WHERE c.clie_limite_credito >= 1000
 ORDER BY c.clie_codigo
+
+-- 2) Mostrar el código, detalle de todos los artículos vendidos en el año 2012 ordenados por cantidad vendida.
+SELECT p.prod_codigo, p.prod_detalle
+FROM Factura f
+INNER JOIN Item_Factura i_f ON (f.fact_tipo = i_f.item_tipo AND f.fact_sucursal = i_f.item_sucursal and f.fact_numero = i_f.item_numero)
+INNER JOIN Producto p ON (p.prod_codigo = i_f.item_producto)
+WHERE YEAR(f.fact_fecha) = 2012
+ORDER BY i_f.item_cantidad
