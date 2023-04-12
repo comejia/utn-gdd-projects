dac5735f31dcebd1e3349d3dd7c3e908e70ff9d5
Cesar <cmejia@frba.utn.edu.ar>
2023-04-12 19:23:34 -0300

    Ej 1

---
+++ Productos/productos_sql.sql
@@ -0,0 +1,7 @@
+use Productos
+
+-- 1) Mostrar el código, razón social de todos los clientes cuyo límite de crédito sea mayor o igual a $ 1000 ordenado por código de cliente.
+SELECT c.clie_codigo, c.clie_razon_social
+FROM Cliente c
+WHERE c.clie_limite_credito >= 1000
+ORDER BY c.clie_codigo
