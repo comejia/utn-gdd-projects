c721e551f2b880295ef2a970e1729b80167321d8
Cesar <cmejia@frba.utn.edu.ar>
2023-04-15 19:01:33 -0300

    Notas de clase 3

--- practica-clases/consultas_en_clase.sql
+++ practica-clases/consultas_en_clase.sql
@@ -21,3 +21,45 @@ FROM Empleado e
 JOIN Empleado j ON (e.empl_jefe = j.empl_codigo)
 -- LEFT JOIN Empleado j ON (e.empl_jefe = j.empl_codigo) -- Muestra a Juan Perez como empleado por mas que no tenga jefe
 
+-- Clase 3: 15/04/2023
+
+-- Alternativa al uso de JOIN
+SELECT p.prod_codigo, p.prod_detalle, f.fami_id, f.fami_detalle
+FROM Producto p, Familia f
+WHERE p.prod_familia = f.fami_id
+ORDER BY p.prod_codigo
+-- Nota: no poner el WHERE va a hacer el producto cartesiano de ambas tablas. Pero si se agrega la condicion entonces
+-- el motor realiza la optimizacion necesario para que la consulta sea igual que con un JOIN
+
+-- Right join: trae la interseccion y las filas de la tabla derecha
+SELECT p.prod_codigo, p.prod_detalle, f.fami_id, f.fami_detalle
+FROM Producto p
+RIGHT JOIN Familia f ON (p.prod_familia = f.fami_id)
+ORDER BY p.prod_codigo 
+
+
+-- Funciones de grupo VERRRRRR VIDEOOOOOOOOOOOOOOO
+SELECT f.fami_id, f.fami_detalle, count(*)
+FROM Producto p
+JOIN Familia f ON p.prod_familia = f.fami_id
+GROUP BY f.fami_id, f.fami_detalle
+
+-- count(campo): cuenta solo si el campo es no nulo
+-- count(*): cuenta si hay al menos un campo no nulo
+-- count(constante): cuenta las filas sin importar si hay todo nulo
+
+-- Otras funciones de grupo:
+-- SUM(campo)
+-- AVG(campo)
+-- MIN(campo)
+-- MAX(campo)
+-- COUNT(DISTINCT campo)
+
+-- HAVING
+-- Sirve para filtrar a nivel de grupo, es decir se usa en conjunto con el GROUP BY (el WHERE filtra a nivel de filas de las tablas
+-- del producto cartesiano)
+
+
+
+
+
